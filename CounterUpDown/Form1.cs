d8db832c5f25c09c4f4bac7eb187abe749fec59e
abdulrahmanbaderalotaibi <opito51@icloud.com>
2024-05-12 08:50:31 +0300

    abdulrahman bader alotaibi 44459057 (669)

--- CounterUpDown/Form1.cs
+++ CounterUpDown/Form1.cs
@@ -6,5 +6,32 @@ namespace CounterUpDown
         {
             InitializeComponent();
         }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, res;
+            n1 = double.Parse(textBox1.Text);
+            n2 = double.Parse(textBox1.Text);
+            res = n1 + 1;
+            textBox1.Text = res.ToString();
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, res;
+            n1 = double.Parse(textBox1.Text);
+            n2 = double.Parse(textBox1.Text);
+            res = n2 - 1;
+            textBox1.Text = res.ToString();
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, res;
+            n1 = double.Parse(textBox1.Text);
+            n2 = double.Parse(textBox1.Text);
+            res = n3 = 0;
+            textBox1.Text = res.ToString();
+        }
     }
 }
